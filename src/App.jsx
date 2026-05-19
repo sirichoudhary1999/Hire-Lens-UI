7f145710f57ddceb92a7c428d13c1636540ad54e
Lekkala <sirisha.lekkala@unisys.com>
2026-05-19 19:56:34 +0530

    added resume feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Login from './Components/Login/Login.jsx';
-import Dashboard from './Components/Dashboard/Dashboard.jsx';
-import UserProfile from "./Components/UserProfile/UserProfile.jsx";
-import JobTracker from "./Components/JobTracker/JobTracker.jsx";
-import AddJob from "./Components/JobTracker/JobOperations/AddorUpdateJob.jsx";
-import ViewJobs from "./Components/JobTracker/JobOperations/ViewJob.jsx"
+import Login from './Login/Login.jsx';
+import Dashboard from './Dashboard/Dashboard.jsx';
+import UserProfile from "./UserProfile/UserProfile.jsx";
+import JobTracker from "./JobTracker/JobTracker.jsx";
+import AddJob from "./JobTracker/JobOperations/AddorUpdateJob.jsx";
+import ViewJobs from "./JobTracker/JobOperations/ViewJob.jsx";
+import Resume from "./Resume/Resume.jsx";
+import UploadResume from "./Resume/ResumeOperations/UploadResume.jsx";
+import ViewResumes from "./Resume/ResumeOperations/ViewResumes.jsx";
+import ViewResumeDetail from "./Resume/ResumeOperations/ViewResumeDetail.jsx";
+import EditResume from "./Resume/ResumeOperations/EditResume.jsx";
+import OptimizeResume from "./Resume/ResumeOperations/OptimizeResume.jsx";
 
 const App = () => {
-
 
   return (
     <div className="App">
@@ -18,9 +23,15 @@ const App = () => {
         <Route path='/login' element={<Login />}/>
         <Route path='/dashboard' element={<Dashboard />}/>
         <Route path='/profile/update' element={<UserProfile />}/>
-        <Route path ="/jobs" element={<JobTracker />} />
-        <Route path = "/jobs/add" element={<AddJob />} />
-        <Route path ="/jobs/view" element={<ViewJobs/>}/>
+        <Route path="/jobs" element={<JobTracker />} />
+        <Route path="/jobs/add" element={<AddJob />} />
+        <Route path="/jobs/view" element={<ViewJobs/>}/>
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/resume/upload" element={<UploadResume />} />
+        <Route path="/resume/view" element={<ViewResumes />} />
+        <Route path="/resume/view/:resumeId" element={<ViewResumeDetail />} />
+        <Route path="/resume/edit/:resumeId" element={<EditResume />} />
+        <Route path="/resume/optimize/:resumeId?" element={<OptimizeResume />} />
       </Routes>
       </BrowserRouter>
     </div>
